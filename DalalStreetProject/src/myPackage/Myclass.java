256e5a468c319b2d8ed0de4cf3820bf5ddb18e2e
Amar Waghmare <amarwaghmare573@gmail.com>
2023-01-05 19:19:33 +0530

    Basic Steps
    Hi Team,
    
    I have created framework structure go head and clone it start with your task.
    Thanks.

---
+++ DalalStreetProject/src/myPackage/Myclass.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+public class Myclass {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Added dependencies");
+		System.out.println("Extent report");
+		System.out.println("Log4j");
+		System.out.println("Basic stuff");
+	}
+
+}
